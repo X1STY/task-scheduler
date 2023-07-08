c121324f64d68b4b12bc3363718fd95dfb5bacd7
X1STY <x1stygg@gmail.com>
2023-07-08 20:54:50 +0700

    add hover color to Buttons

--- src/utils/api/instance.js
+++ src/utils/api/instance.js
@@ -1,10 +1,10 @@
-import axios from "axios";
-const accessToken = localStorage.getItem('accessToken')
-//Kurator, can I import useContext(AuthContext) here to get token from context like auth?.token ?
+import axios from 'axios';
+const accessToken = localStorage.getItem('accessToken');
+
 const instance = axios.create({
-    baseURL: 'https://64a4f83300c3559aa9beddb2.mockapi.io/api/',
-  });
+  baseURL: 'https://64a4f83300c3559aa9beddb2.mockapi.io/api/'
+});
 
 instance.defaults.headers.common['Authorization'] = accessToken;
 
-export default instance
+export default instance;
