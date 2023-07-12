bf7d087a673e922d9320cbadae720a73b766f307
Xcution <xcution.js@gmail.com>
2023-07-12 11:19:01 +0700

    added login form logic and tested on mock

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,20 +1,63 @@
+import {useState} from 'react';
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { useForm, Controller, useFormState } from "react-hook-form";
+import { useMutation } from '@tanstack/react-query';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useNavigate } from 'react-router-dom';
-import { useForm, Controller, useFormState } from "react-hook-form";
-import './Login.css';
 import {emailValidation, passwordValidation} from "./validation.js";
-import React from 'react';
-
+import './Login.css';
 
 const Login = () => {
     const {handleSubmit, control} = useForm();
     const { errors } = useFormState({
         control
     });
+    const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate()
-    const onSubmit = (data) => console.log(data);
+
+    const mutation = useMutation((data) =>
+        axios.post('https://64ad54beb470006a5ec5c676.mockapi.io/api/posts', data)
+    );
+
+    const onSubmit = async (data) => {
+        try {
+            await mutation.mutateAsync(data);
+            const token = mutation.data.token;
+            console.log(data);
+            console.log(mutation.data);
+            console.log(token);
+            if (token) {
+                localStorage.setItem('token', token);
+            }
+            navigate('/scheduler');
+        }  catch (error) {
+            if (error.response) {
+                const { status } = error.response;
+                let errorMessage = '';
+
+                switch (status) {
+                    case 400:
+                        errorMessage = 'Некорректный ввод электронной почты или пароля';
+                        break;
+                    case 401:
+                        errorMessage = 'Неверный адрес электронной почты или пароль';
+                        break;
+                    case 500:
+                        errorMessage = 'Внутренняя ошибка сервера';
+                        break;
+                    default:
+                        errorMessage = 'Произошла ошибка';
+                        break;
+                }
+
+                setErrorMessage(errorMessage);
+            } else {
+                setErrorMessage('Произошла ошибка');
+            }
+        }
+    };
 
     return (
         <div className = "login-form">
