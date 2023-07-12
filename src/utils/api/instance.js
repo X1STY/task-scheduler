143e8deea890964cff39006e2c5478b6df4e81cc
X1STY <x1stygg@gmail.com>
2023-07-12 12:21:14 +0700

    change headers

--- src/utils/api/instance.js
+++ src/utils/api/instance.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
-const accessToken = localStorage.getItem('accessToken');
+const token = localStorage.getItem('accessToken');
 
 const instance = axios.create({
-  // baseURL: 'https://64a4f83300c3559aa9beddb2.mockapi.io/api/'
-  baseURL: 'http://81.177.197.88:8080/api/'
+  baseURL: 'https://64a4f83300c3559aa9beddb2.mockapi.io/api/'
+  
 });
 
-instance.defaults.headers.common['Authorization'] = accessToken;
-instance.defaults.headers.common['Content-Type'] = 'application/json';
+export const headers = {
+  'Content-Type': 'application/json',
+  token: `${token}`
+};
 
 export default instance;
