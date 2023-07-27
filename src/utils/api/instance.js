90bcf3769e2fe29eff19c3ea0f471bed18f5f140
X1STY <x1stygg@gmail.com>
2023-07-27 21:17:38 +0700

    change api to localhost in case of extend app

--- src/utils/api/instance.js
+++ src/utils/api/instance.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'https://64a4f83300c3559aa9beddb2.mockapi.io/api/'
-  baseURL: 'http://81.177.197.88:8080/api/'
+  // baseURL: 'http://81.177.197.88:8080/api/'
+  baseURL: 'http://localhost:1111/api'
 });
 
 instance.defaults.headers.common['access-token'] = `${localStorage.getItem('token')}`;
--- src/utils/requests/UserEvents.js
+++ src/utils/requests/UserEvents.js
@@ -3,7 +3,7 @@ import instance from '../api/instance';
 export const getUserEvents = async (startDate, endDate) => {
   return await instance.get(`/events?start_date=${startDate}&end_date=${endDate}`, {
     headers: {
-      'access-token': `${localStorage.getItem('token')}`
+      access_token: `${localStorage.getItem('token')}`
     }
   });
 };
@@ -11,7 +11,7 @@ export const getUserEvents = async (startDate, endDate) => {
 export const addNewEvent = async (newEvent) => {
   return await instance.post('/event', newEvent, {
     headers: {
-      'access-token': `${localStorage.getItem('token')}`
+      access_token: `${localStorage.getItem('token')}`
     }
   });
 };
@@ -19,7 +19,7 @@ export const addNewEvent = async (newEvent) => {
 export const addNewEventWithReply = async (newEvent) => {
   return await instance.post('/event-group', newEvent, {
     headers: {
-      'access-token': `${localStorage.getItem('token')}`
+      access_token: `${localStorage.getItem('token')}`
     }
   });
 };
@@ -27,7 +27,15 @@ export const addNewEventWithReply = async (newEvent) => {
 export const deleteEventById = async (id) => {
   return await instance.delete(`/event/${id}`, {
     headers: {
-      'access-token': `${localStorage.getItem('token')}`
+      access_token: `${localStorage.getItem('token')}`
+    }
+  });
+};
+
+export const deleteEventGroup = async (id) => {
+  return await instance.delete(`/event-group/${id}`, {
+    headers: {
+      access_token: `${localStorage.getItem('token')}`
     }
   });
 };
@@ -35,7 +43,15 @@ export const deleteEventById = async (id) => {
 export const changeEventById = async (body) => {
   return await instance.patch(`/event/${body.id}`, body.updatedEvent, {
     headers: {
-      'access-token': `${localStorage.getItem('token')}`
+      access_token: `${localStorage.getItem('token')}`
+    }
+  });
+};
+
+export const changeEventGroup = async (body) => {
+  return await instance.patch(`/event-group/${body.event_group_id}`, body.updatedEvent, {
+    headers: {
+      access_token: `${localStorage.getItem('token')}`
     }
   });
 };
